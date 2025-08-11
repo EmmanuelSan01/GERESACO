8ed5d821e267baeb22a1b1549b4c0b4fdb903a6e
EmmanuelSan01 <emmanuel.sanchez.estepa@gmail.com>
2025-08-11 15:33:32 -0500

    CC-1011 Add RoomsRoutes

--- app/main.py
+++ app/main.py
@@ -14,12 +14,14 @@ if PROJECT_ROOT not in sys.path:
 
 from backend.core.db import create_db_and_tables
 from backend.routes.users.UsersRoutes import router as users_router
+from backend.routes.rooms.RoomsRoutes import router as rooms_router
 
 # Crear app
 app = FastAPI(title="GERESACO API", version="1.0.0")
 
 # Incluir rutas
 app.include_router(users_router)
+app.include_router(rooms_router)
 
 @app.on_event("startup")
 def on_startup():
@@ -29,11 +31,6 @@ def on_startup():
 @app.get("/")
 def health_check():
     return {"status": "ok"}
-
-# Mostrar rutas cargadas para depuración
-@app.on_event("startup")
-def show_routes():
-    print("Rutas cargadas:", [route.path for route in app.routes])
 
 if __name__ == "__main__":
     import uvicorn
